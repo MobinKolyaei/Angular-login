578510f8df71db310cd9d4ff3b2fd258235f3a70
Mobin <mobinkolyaei@gmail.com>
2023-04-25 00:54:25 +0430

    base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { getMatInputUnsupportedTypeError } from '@angular/material/input';
+import { disableDebugTools } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'B';
+  person = {
+    firstname:'',
+    lastname:'',
+    gender:'',
+    nezam:'',
+    married:false,
+    children:0,
+  };
+  cancel(){
+    this.person.firstname='';
+    this.person.lastname='';
+    this.person.gender='';
+    this.person.nezam='';
+    this.person.married=false;
+    this.person.children=0;
+  }
+  key(e:KeyboardEvent){
+    if (e.key>='0' && e.key<='9') {
+      e.preventDefault();
+    }
+    console.log(e);
+  }
+  number(n:KeyboardEvent){
+    if(n.key.includes('-') || n.key.includes('.') || n.key.includes('+')){
+      n.preventDefault();
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    MatInputModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    FormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
